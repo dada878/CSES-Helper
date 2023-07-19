3ca19db6c231676b3e8a058f4349467b7be9b2a7
Yu-Ren Pan <YuRen.Pan.0117@gmail.com>
2023-07-20 02:27:33 +0800

    修 Promise

--- scripts/background.js
+++ scripts/background.js
@@ -1,31 +1,20 @@
 if (typeof browser === "undefined") var browser = chrome;
 
-const getFileFromGithub = (path) => {
-    return new Promise((resolve, reject) => {
-        fetch(path)
-            .then(response => response.json())
-            .then(data => {
-                const content = data.payload.blob.rawLines.join("\n");
-                resolve(JSON.parse(content));
-            });
-    });
-};
-
-const getTags = (problemId) => {
-    return new Promise((resolve, reject) => {
-        getFileFromGithub("https://github.com/dada878/CSES-Helper/blob/master/database/tags.json").then((tagsData) => {
-            resolve(tagsData[problemId]);
+const getFileFromGithub = (path) =>
+    fetch(path)
+        .then(response => response.json())
+        .then(data => {
+            const content = data.payload.blob.rawLines.join("\n");
+            return JSON.parse(content);
         });
-    });
-};
 
-const getTips = (problemId) => {
-    return new Promise((resolve, reject) => {
-        const tipsData = getFileFromGithub("https://github.com/dada878/CSES-Helper/blob/master/database/tips.json").then((tipsData) => {
-            resolve(tipsData[problemId]);
-        });
-    });
-};
+const getTags = (problemId) =>
+    getFileFromGithub("https://github.com/dada878/CSES-Helper/blob/master/database/tags.json")
+        .then(tagsData => tagsData[problemId]);
+
+const getTips = (problemId) =>
+    getFileFromGithub("https://github.com/dada878/CSES-Helper/blob/master/database/tips.json")
+        .then(tipsData => tipsData[problemId]);
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.command === "fetch-tags") {
