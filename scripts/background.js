6e83466bdc25ca4ac6f66a0caa0faa71763a3b43
Dada878 <xx0932399@gmail.com>
2023-07-19 09:24:58 +0800

    初步完成 Tags 功能

--- scripts/background.js
+++ scripts/background.js
@@ -1,4 +1,24 @@
-fetch("https://github.com/official-scint/official-scint.github.io/blob/master/config.json").then(response => response.json()).then(data => {
-    const content = data.payload.blob.rawLines.join("\n");
-    console.log(JSON.parse(content));
+if (typeof browser === "undefined") var browser = chrome;
+
+const getTags = (problemId) => {
+    return new Promise((resolve, reject) => {
+        fetch("https://github.com/dada878/CSES-Helper/blob/master/database/tags.json")
+            .then(response => response.json())
+            .then(data => {
+                const content = data.payload.blob.rawLines.join("\n");
+                tagsData = JSON.parse(content);
+                resolve(tagsData[problemId]);
+        });
+    });
+};
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.command === "fetch-tags") {
+        getTags(request.problemId).then((tags) => {
+            sendResponse({
+                tags: tags ?? []
+            });
+        });
+    }
+    return true;
 });
--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,33 @@
+if (typeof browser === "undefined") var browser = chrome;
+
+const getTags = async (problemId) => {
+    return new Promise((resolve, reject) => {
+        browser.runtime.sendMessage({
+            command: "fetch-tags",
+            problemId: problemId
+        }).then((response) => {
+            resolve(response.tags);
+        });
+    });
+}
+
+const problemId = window.location.href.split("/").at(-1);
 const navbarElement = document.querySelector(".nav");
 const sidebarElement = document.querySelector(".nav.sidebar");
 
-const createTagsSectionOnSidebar = () => {
+const createTagsSectionOnSidebar = async () => {
     const dividerLine = document.createElement("hr");
     const sectionTitle = document.createElement("h4");
     sectionTitle.innerHTML = "Tags";
     const showTags = document.createElement("details");
     const showTagsSummary = document.createElement("summary");
-    const tagsList = ["BIT", "sort", "set"];
-    const tagsListElement = document.createElement("p");
-    tagsListElement.innerHTML = tagsList.join("  ");
+    const tagsList = await getTags(problemId);
+    const tagsListElement = document.createElement("ul");
+    tagsList.forEach((tag) => {
+        const tagElement = document.createElement("li");
+        tagElement.innerHTML = tag;
+        tagsListElement.appendChild(tagElement);
+    });
     showTags.appendChild(tagsListElement);
     showTagsSummary.innerHTML = "Show Problem Tags";
     showTags.appendChild(showTagsSummary);
@@ -19,5 +37,3 @@ const createTagsSectionOnSidebar = () => {
 }
 
 createTagsSectionOnSidebar();
-
-fetch("https://github.com/official-scint/official-scint.github.io/blob/master/config.json");
